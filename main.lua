4ecf8e8874172a9274651c44062a5ae45f59b027
Marc-Olivier Fleury <mofleury@gmail.com>
2016-12-24 10:51:13 +0100

    using natural coordinates instead of top-down coordinates

--- main.lua
+++ main.lua
@@ -1,32 +1,46 @@
 platform = {}
 player = {}
- 
+
+obstacles= {}
+
+screen = {}
+
 function love.load()
-	platform.width = love.graphics.getWidth()
-	platform.height = love.graphics.getHeight()
- 
+	if arg[#arg] == "-debug" then require("mobdebug").start() end
+
+	screen.dx = love.graphics.getWidth()
+	screen.dy = love.graphics.getHeight()
+
+	platform.dx = love.graphics.getWidth()
+	platform.dy = 20
+
 	platform.x = 0
-	platform.y = platform.height / 2
- 
-	player.x = love.graphics.getWidth() / 2
-	player.y = love.graphics.getHeight() / 2
- 
+	platform.y = 0
+
+	player.x = screen.dx / 2
+	player.y = platform.y + platform.dy
+	player.dx = 32
+	player.dy = 32
+
 	player.speed = 200
  
 	player.img = love.graphics.newImage('resources/purple.png')
  
-	player.ground = player.y
+	player.ground = player.y-1
  
 	player.y_velocity = 0
  
-	player.jump_height = -300
+	player.jump_height = 300
 	player.gravity = -800
+
+	table.insert(obstacles, {x=10, y=30, dx=10, dy=20})
+
 end
  
 function love.update(dt)
 
 	if love.keyboard.isDown('d') then
-		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
+		if player.x < (screen.dx - player.dx) then
 			player.x = player.x + (player.speed * dt)
 		end
 	elseif love.keyboard.isDown('a') then
@@ -39,25 +53,37 @@ function love.update(dt)
 		if player.y_velocity == 0 then
 			player.y_velocity = player.jump_height
 		end
-	elseif player.y_velocity < 0 then
+	elseif player.y_velocity > 0 then
 		-- mid jump, but not hitting jum key anymore : small jump
 		player.y_velocity = 0
 	end
  
-	if player.y > player.ground ~= 0 then
+	if player.y >= player.ground then
 		player.y = player.y + player.y_velocity * dt
-		player.y_velocity = player.y_velocity - player.gravity * dt
+		player.y_velocity = player.y_velocity + player.gravity * dt
 	end
  
-	if player.y > player.ground then
+	if player.y <= player.ground then
 		player.y_velocity = 0
     	player.y = player.ground
 	end
 end
- 
+
+
+local function drawBox(b)
+	love.graphics.setColor(200, 200, 200)
+	love.graphics.rectangle('fill', b.x, screen.dy- b.y -b.dy, b.dx, b.dy)
+end
+
 function love.draw()
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.rectangle('fill', platform.x, platform.y, platform.width, platform.height)
- 
-	love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 32)
+
+
+	drawBox(platform)
+
+	drawBox(player)
+	-- love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 32)
+
+	for i,o in ipairs(obstacles) do
+		drawBox(o)
+	end
 end
