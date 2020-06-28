8af313267d91c8c5f227415374583e4629b5a343
Marc-Olivier Fleury <mofleury@gmail.com>
2020-06-28 19:16:30 +0200

    improved walker behaviot, to switch between approach and patrol

--- controllers/walker.lua
+++ controllers/walker.lua
@@ -7,6 +7,11 @@ local speed = 1
 
 local approach_distance = 100
 
+local detect_distance = 200
+
+local wander_duration = 1.5
+local pause_duration = 0.5
+
 local behaviors = {}
 
 function brain.newInstance(origin)
@@ -24,23 +29,64 @@ function brain.newInstance(origin)
 
     walker.orientation = 1
 
-    walker.behavior = behaviors.approach
+    walker.behavior = behaviors.patrol
+
+    walker.last_transition = 0
+    walker.next_transiton = 0
 
     return walker
 end
 
+local function move_if_possible(walker, new_x, map)
+    local has_footing = control.has_footing(walker, new_x, map)
 
-function behaviors.approach(walker, player, map)
+    if (has_footing) then
+        walker.x = new_x
+        return true
+    end
+
+    return false
+end
+
+function behaviors.approach(walker, player, map, dt)
     walker.orientation = control.sign(player.x - walker.x)
 
     local distance = math.abs(player.x - walker.x)
 
     local new_x = walker.x + speed * walker.orientation
 
-local has_footing = control.has_footing(walker, new_x, map)
+    if (distance > approach_distance) then
+        move_if_possible(walker, new_x, map)
+    end
 
-    if (distance > approach_distance and has_footing) then
-        walker.x = new_x
+    if distance > detect_distance then
+        return behaviors.pause
+    else
+        return behaviors.approach
+    end
+end
+
+function behaviors.pause(walker, player, map, dt)
+    if walker.last_transition > pause_duration then
+        walker.orientation = -1 * walker.orientation
+        return behaviors.patrol
+    else
+        return behaviors.pause
+    end
+end
+
+function behaviors.patrol(walker, player, map, dt)
+
+
+    if walker.last_transition > wander_duration then
+        return behaviors.pause
+    else
+        local new_x = walker.x + speed * walker.orientation
+        if move_if_possible(walker, new_x, map) then
+            return behaviors.patrol
+        else
+            return behaviors.pause
+        end
     end
 end
 
@@ -51,11 +97,26 @@ function brain.controller(walker, player, map, screen)
 
     function controller.update(dt)
 
-       local old_x = walker.x
+        local old_x = walker.x
 
-        walker.behavior(walker, player, map)
+        local distance = math.abs(player.x - walker.x)
 
-       control.constrain_walker_update(walker, map, dt)
+
+        local new_behavior = nil
+
+        if distance < detect_distance then
+            walker.behavior = behaviors.approach
+        end
+
+        walker.last_transition = walker.last_transition + dt
+
+        new_behavior = walker.behavior(walker, player, map, dt)
+        if new_behavior ~= walker.behavior then
+            walker.behavior = new_behavior
+            walker.last_transition = 0
+        end
+
+        control.constrain_walker_update(walker, map, dt)
 
         if old_x ~= walker.x then
             walker.state = "walking"
